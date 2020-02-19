cc92557bb9a9f27bbaed9aff1988760e3f0ffee9
Kavish Rizvi <rizvikavish205@gmail.com>
2020-02-19 20:22:24 +0530

    Initial Commit

---
+++ src/test/java/com/sapient/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
